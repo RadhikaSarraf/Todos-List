bb53931e22f928fc98d08510840ab4ce016d4f6a
Radhika Sarraf <radhikasarraf1808@gmail.com>
2022-02-01 23:45:49 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about Component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel qui dolor laborum illum ipsum. Voluptas, dolorem harum, voluptatibus placeat dolore nesciunt iure necessitatibus in et similique sint natus beatae recusandae deserunt quis consequuntur nihil.</p>
+        </div>
+    )
+}
